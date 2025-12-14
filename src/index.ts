bbf0cddaa8c78078ac7681abc7e505901ed2be34
3umaGH <3uma+github@protonmail.com>
2025-12-14 14:31:19 +0200

    refactor: rename to generic PubSub wrapper/interface. Add unsub method.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { RedisInterface, WrapperEvents } from './types/redis'
+import { PubSubInterface, WrapperEvents, WrapperReturnType } from './types'
 
-const wrapRedis = <E extends WrapperEvents>(clientInterface: RedisInterface | (() => RedisInterface)) => {
+export const wrapPubSub = <E extends WrapperEvents>(
+  clientInterface: PubSubInterface | (() => PubSubInterface)
+): WrapperReturnType<E> => {
+  const subs: Record<string, unknown[]> = {} // channel -> events[]
+
   return {
     publish: async <K extends keyof E>(channel: string, event: K, payload: E[K]) => {
       const client = typeof clientInterface === 'function' ? clientInterface() : clientInterface
@@ -9,16 +13,42 @@ const wrapRedis = <E extends WrapperEvents>(clientInterface: RedisInterface | ((
     },
     subscribe: async <K extends keyof E>(channel: string, event: K, handler: (message: E[K]) => void) => {
       const client = typeof clientInterface === 'function' ? clientInterface() : clientInterface
+
+      if (!subs[channel]) {
+        subs[channel] = []
+      }
+
+      subs[channel].push(event)
+
       await client.subscribe(channel, (message: string) => {
         try {
           const parsed: { event: K; payload: E[K] } = JSON.parse(message)
           if (parsed.event === event) {
+            if (!subs[channel]?.includes(event)) {
+              return
+            }
+
             handler(parsed.payload)
           }
         } catch (error) {
           console.error('Error parsing message:', error)
         }
       })
+    },
+    unsubscribe: async <K extends keyof E>(channel: string, event: K) => {
+      const client = typeof clientInterface === 'function' ? clientInterface() : clientInterface
+
+      if (subs[channel]) {
+        const eventIndex = subs[channel].indexOf(event)
+        if (eventIndex !== -1) {
+          subs[channel].splice(eventIndex, 1)
+        }
+
+        if (subs[channel].length === 0) {
+          delete subs[channel]
+          await client.unsubscribe(channel)
+        }
+      }
     },
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,13 @@
-export type RedisInterface = {
+export type PubSubInterface = {
   publish: (channel: string, message: string) => Promise<void>
   subscribe: (channel: string, handler: (message: string) => void) => Promise<void>
+  unsubscribe: (channel: string) => Promise<void>
 }
 
 export type WrapperReturnType<E extends WrapperEvents> = {
   publish: <K extends keyof E>(channel: string, event: K, payload: E[K]) => Promise<void>
   subscribe: <K extends keyof E>(channel: string, event: K, handler: (payload: E[K]) => void) => Promise<void>
+  unsubscribe: <K extends keyof E>(channel: string, event: K) => Promise<void>
 }
 
 export type WrapperEvents = Record<string, unknown>
